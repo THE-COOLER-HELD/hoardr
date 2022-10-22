a7b360bf94e973d6d93ca6bae2d7c279aee0ae4a
Emily Bennett <emily.bennett@northcoders.com>
2022-10-22 16:29:00 +0100

    adding AddExpense page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import IncomePage from './components/IncomePage/IncomePage'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import IncomePage from "./components/IncomePage/IncomePage";
+import AddExpense from "./components/AddExpense/AddExpense";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <IncomePage />
+      <AddExpense />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
