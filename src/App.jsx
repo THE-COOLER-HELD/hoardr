02df55047acae53fea4a904f6cf3829a15500d3f
hevmag <90092645+hevmag@users.noreply.github.com>
2022-10-23 14:32:25 +0100

    fix user pathway

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
+import { useState, useContext } from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage/Homepage';
+import SignupForm from './components/SignupForm/SignupForm';
+import SignupLogin from './components/SignupLogin/SignupLogin';
+import LoginPage from './components/LoginPage/LoginPage';
 
-import { useState, useContext } from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Homepage from "./components/Homepage/Homepage";
-import SignupForm from "./components/SignupForm/SignupForm";
-import SignupLogin from "./components/SignupLogin/SignupLogin";
-import LoginPage from "./components/LoginPage/LoginPage";
-
-import NavMenu from "./components/NavMenu/NavMenu.jsx";
-import AddIncome from "./components/AddIncome/AddIncome";
-import ViewExpenses from "./components/ViewExpenses/ViewExpenses";
-import UserContext from "./contexts/UserContext";
-import AddExpense from "./components/AddExpense/AddExpense";
+import NavMenu from './components/NavMenu/NavMenu.jsx';
+import AddIncome from './components/AddIncome/AddIncome';
+import ViewExpenses from './components/ViewExpenses/ViewExpenses';
+import UserContext from './contexts/UserContext';
+import AddExpense from './components/AddExpense/AddExpense';
 
 function App() {
 	const [user, setUser] = useState({});
-  console.log(user)
+	console.log(user);
 	return (
 		<UserContext.Provider value={{ user, setUser }}>
-			<div className='App'>
-				<NavMenu />
+			<div className="App">
+				<NavMenu user={user} />
 				<Routes>
-					<Route path='/' element={<SignupLogin />} />
-					<Route path='/signup' element={<SignupForm />} />
-					<Route path='/login' element={<LoginPage />} />
-					<Route path='/expenses' element={<ViewExpenses />} />
-					<Route path='/add' element={<AddIncome />} />
+					{!user.id ? (
+						<>
+							<Route path="/" element={<SignupLogin />} />
+							<Route path="/signup" element={<SignupForm />} />
+							<Route path="/login" element={<LoginPage />} />
+						</>
+					) : (
+							<>	
+								<Route path="/" element={<Homepage />} />
+							<Route
+								path="/expenses"
+								element={<ViewExpenses />}
+							/>
+							<Route path="/add" element={<AddIncome />} />
+						</>
+					)}
 				</Routes>
 			</div>
 		</UserContext.Provider>
 	);
-
 }
 
 export default App;
