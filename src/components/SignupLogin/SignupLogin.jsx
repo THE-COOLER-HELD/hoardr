ae1fb90e87333f62fd16ee420589ac8900bfe9ae
Emily Bennett <emily.bennett@northcoders.com>
2022-10-23 14:36:24 +0100

    signup login change pathway

--- src/components/SignupLogin/SignupLogin.jsx
+++ src/components/SignupLogin/SignupLogin.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import SignupForm from "../SignupForm/SignupForm";
-import Homepage from "../Homepage/Homepage";
+
 import MagicSignUp from "../MagicSignUp/MagicSignUp";
 import useSignupLogin from "../../hooks/useSignupLogin";
 
 function SignupLogin() {
-	const { session, isSignedUp } = useSignupLogin()
-	return (
-		<div>
-			{isSignedUp ? (
-				<Homepage session={session} />
-			) : session ? (
-				<SignupForm session={session} />
-			) : (
-				<MagicSignUp />
-			)}
-		</div>
-	);
+  const { session, isSignedUp } = useSignupLogin();
+  return (
+    <div>
+      {!session ? <MagicSignUp /> : !isSignedUp ? <SignupForm /> : null}
+    </div>
+  );
 }
 
 export default SignupLogin;
