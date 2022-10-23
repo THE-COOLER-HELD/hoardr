d1f28c949e555cfb11df838a76d5dbb6e1bfa345
DCrawley94 <duncan.crawley@gmail.com>
2022-10-23 16:29:04 +0100

    fix addIncome bug

--- src/components/AddIncome/AddIncome.jsx
+++ src/components/AddIncome/AddIncome.jsx
@@ -2,23 +2,34 @@ import React from "react";
 import useAddIncome from "../../hooks/useAddIncome";
 
 function AddIncome() {
-  // const { onChange, onSubmit } = useAddIncome()
-  
-  return (
-    <div className="add-income-modal">
-      <h1 className="income-head">Add to your Hoard</h1>
-      <form className="add-income-form">
-        <label> How much?</label>
-        <input type="number" aria-label="amount of money to add"></input>
+	const { onChange, onSubmit } = useAddIncome();
 
-        <label>When did you acquire this?</label>
-        <input type="date" aria-label="date of income"></input>
+	return (
+		<div className='add-income-modal'>
+			<h1 className='income-head'>Add to your Hoard</h1>
+			<form onSubmit={onSubmit} className='add-income-form'>
+				<label> How much?</label>
+				<input
+					onChange={(event) => {
+						onChange("amount", event.target.value);
+					}}
+					type='number'
+					aria-label='amount of money to add'
+				></input>
 
+				<label>When did you acquire this?</label>
+				<input
+					onChange={(event) => {
+						onChange("date", event.target.value);
+					}}
+					type='date'
+					aria-label='date of income'
+				></input>
 
-        <button className="add-hoard-button">Add to hoard</button>
-      </form>
-    </div>
-  );
+				<button className='add-hoard-button'>Add to hoard</button>
+			</form>
+		</div>
+	);
 }
 
 export default AddIncome;
